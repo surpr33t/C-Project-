5917b3cbc2955b8268819e6e8b5420f3bde40317
mquraish05 <mquraish@usc.edu>
2025-04-25 11:49:41 -0700

    Update ApplicationEvent.cpp

--- ApplicationEvent.cpp
+++ ApplicationEvent.cpp
@@ -15,3 +15,14 @@ string ApplicationEvent::getNotes() const {
     return notes_;
 }
 
+//child class that says when it was submitted 
+ResumeSubmissionEvent::ResumeSubmissionEvent(const string& date, const string& notes)
+    : ApplicationEvent(date, notes) {}
+
+string ResumeSubmissionEvent::getType() const {
+    return "Resume Submission";
+}
+
+void ResumeSubmissionEvent::printEvent() const {
+    cout << "Resume submitted on " << date_ << ". Notes: " << notes_ << endl;
+}
