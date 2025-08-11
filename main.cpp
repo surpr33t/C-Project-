9e3b1c1152721b27a4a962c8a999c21ef1fb27fa
surpr33t <surpreet@usc.edu>
2025-08-11 11:33:48 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,13 @@ int main() {
     Company usc("University of Southern California", "Los Angeles, CA", "https://usc.edu");
 
     Application app(usc, "Research Assistant", "2025-05-01");
-
     app.addEvent(new ResumeSubmissionEvent("2025-05-02", "Submitted via USC careers portal"));
     app.addEvent(new InterviewEvent("2025-05-04", "Zoom interview with faculty advisor", InterviewFormat::VIDEO));
     app.addEvent(new OfferEvent("2025-05-10", "Offer extended via email", 42000.00));
 
     app.updateStatus("Offer Received");
-
     app.printSummary();
 
     return 0;
 }
+
